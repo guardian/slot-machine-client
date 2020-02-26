8af2fc3dce6e74303d016d6689fbd7be8157cd19
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-02-26 17:06:23 +0000

    Extend getBodyEnd function accept optional api endpoint override

--- client.ts
+++ client.ts
@@ -2,9 +2,9 @@ import { Metadata } from "./types";
 
 const apiURL = "https://contributions.guardianapis.com/epic";
 
-export const getBodyEnd = (meta: Metadata): Promise<Response> => {
+export const getBodyEnd = (meta: Metadata, useUrl?: string): Promise<Response> => {
   const json = JSON.stringify(meta);
-  return fetch(apiURL, {
+  return fetch(useUrl || apiURL, {
     method: "post",
     headers: { "Content-Type": "application/json" },
     body: json
