16b3db1dfa8b6c9fcb7bdbcf7ea7c7fb5a281c73
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-02-27 14:42:47 +0000

    Uses url for fetch url

--- client.ts
+++ client.ts
@@ -4,7 +4,7 @@ const apiURL = "https://contributions.guardianapis.com/epic";
 
 export const getBodyEnd = (meta: Metadata, url: string = apiURL): Promise<Response> => {
   const json = JSON.stringify(meta);
-  return fetch(useUrl || apiURL, {
+  return fetch(url, {
     method: "post",
     headers: { "Content-Type": "application/json" },
     body: json
