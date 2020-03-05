0cd94ccb01c6fcc89feaf0e283a360a6551a0870
Tom Wey <tom.wey@theguardian.com>
2020-03-05 11:49:21 +0000

    Remove unused plugin
    I'd missed a warning from eslint that the relevant lib couldn't be
    found.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
-    ],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module',
