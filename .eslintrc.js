dd41fcb6b75ad60d93e1a634a1dd2969c2bdb820
Tom Wey <tom.wey@theguardian.com>
2020-03-05 11:27:29 +0000

    Prettier/eslint config
    Inherited and adapted from contributions-service.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {},
+    },
+    rules: {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        curly: 2,
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
