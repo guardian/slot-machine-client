3af6266c2a6ca5140f58b00c7df5d8ccb711f954
Nicolas Long <nicolas.long@theguardian.com>
2020-03-06 09:54:33 +0000

    Format with prettier update

--- contributions/compare.ts
+++ contributions/compare.ts
@@ -1,24 +1,23 @@
-import { Targeting } from "../types";
+import { Targeting } from '../types';
 
 interface CompareMeta {
-  targeting: Targeting;
-  expectedTest: string;
-  expectedVariant: string;
+    targeting: Targeting;
+    expectedTest: string;
+    expectedVariant: string;
 }
 
-const apiURL =
-  "https://contributions.guardianapis.com/epic/compare-variant-decision";
+const apiURL = 'https://contributions.guardianapis.com/epic/compare-variant-decision';
 
 // Temporary helper to evaluate our variant logic server-side
 // No useful response expected (so discard).
 export const compareVariantDecision = (
-  meta: CompareMeta,
-  url: string = apiURL
+    meta: CompareMeta,
+    url: string = apiURL,
 ): Promise<Response> => {
-  const json = JSON.stringify(meta);
-  return fetch(url, {
-    method: "post",
-    headers: { "Content-Type": "application/json" },
-    body: json
-  });
+    const json = JSON.stringify(meta);
+    return fetch(url, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: json,
+    });
 };
