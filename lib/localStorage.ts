4e75c599a59bf2b174b0766f038d0ac0dd13ba4a
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-03-11 16:53:48 +0000

    Run prettier through all files

--- lib/localStorage.ts
+++ lib/localStorage.ts
@@ -2,99 +2,95 @@
 // See original code in Frontend:
 // https://github.com/guardian/frontend/blob/master/static/src/javascripts/lib/storage.js
 const checkAvailability = (): boolean => {
-  const key = 'local-storage-module-test';
-  try {
-    localStorage.setItem(key, 'graun');
-    localStorage.removeItem(key);
-    return true;
-  } catch (e) {
-    return false;
-  }
+    const key = 'local-storage-module-test';
+    try {
+        localStorage.setItem(key, 'graun');
+        localStorage.removeItem(key);
+        return true;
+    } catch (e) {
+        return false;
+    }
 };
 
 const storage = window.localStorage;
 const isAvailable = checkAvailability();
 
 export const get = (key: string): any => {
-  if (!isAvailable) {
-    return null;
-  }
-
-  let data;
-  // Try and parse the data
-  try {
-    const value = getRaw(key);
-
-    if (value === null || value === undefined) {
-      return null;
+    if (!isAvailable) {
+        return null;
     }
 
-    data = JSON.parse(value);
+    let data;
+    // Try and parse the data
+    try {
+        const value = getRaw(key);
 
-    if (data === null) {
-      return null;
+        if (value === null || value === undefined) {
+            return null;
+        }
+
+        data = JSON.parse(value);
+
+        if (data === null) {
+            return null;
+        }
+    } catch (e) {
+        remove(key);
+        return null;
     }
-  } catch (e) {
-    remove(key);
-    return null;
-  }
 
-  // Check if the data has an expiration date and remove it if expired
-  if (data.expires && new Date() > new Date(data.expires)) {
-    remove(key);
-    return null;
-  }
+    // Check if the data has an expiration date and remove it if expired
+    if (data.expires && new Date() > new Date(data.expires)) {
+        remove(key);
+        return null;
+    }
 
-  // If data contains a value property, assume that's what we care about
-  if (typeof data.value !== 'undefined') {
-    return data.value;
-  }
+    // If data contains a value property, assume that's what we care about
+    if (typeof data.value !== 'undefined') {
+        return data.value;
+    }
 
-  // Otherwise return value as is
-  return data;
+    // Otherwise return value as is
+    return data;
 };
 
 const getRaw = (key: string): string | null => {
-  if (isAvailable) {
-    return storage.getItem(key);
-  }
+    if (isAvailable) {
+        return storage.getItem(key);
+    }
 
-  return null;
+    return null;
 };
 
-export const set = (
-  key: string,
-  value: any,
-  options: Record<string, any> = {}
-): void => {
-  if (!isAvailable) {
-    return;
-  }
+export const set = (key: string, value: any, options: Record<string, any> = {}): void => {
+    if (!isAvailable) {
+        return;
+    }
 
-  storage.setItem(
-    key,
-    JSON.stringify({
-      value,
-      expires: options.expires
-    })
-  );
+    storage.setItem(
+        key,
+        JSON.stringify({
+            value,
+            expires: options.expires,
+        }),
+    );
 };
 
 // Prevents any existing value from being overwritten
 export const setIfNotExists = (
-  key: string,
-  value: any,
-  options: Record<string, any> = {}
+    key: string,
+    value: any,
+    options: Record<string, any> = {},
 ): void => {
-  if (getRaw(key) !== null) {
-    return;
-  }
+    if (getRaw(key) !== null) {
+        return;
+    }
 
-  set(key, value, options);
+    set(key, value, options);
 };
 
 export const remove = (key: string): void => {
-  if (isAvailable) {
-    storage.removeItem(key);
-  }
+    if (isAvailable) {
+        storage.removeItem(key);
+    }
 };
