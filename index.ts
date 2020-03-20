b96ed7e6cec3d0e822c84b34263b6385016216ea
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-03-20 19:53:28 +0000

    Adds JS to handle reminder form

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { getBodyEnd } from './slots';
+export { getBodyEnd, initSlot } from './slots';
 export { getWeeklyArticleHistory, incrementWeeklyArticleCount } from './history';
 export { getViewLog, logView, getNumViewsInPreviousDays } from './contributions/viewLog';
 export { compareVariantDecision } from './contributions/compare';
--- slots.ts
+++ slots.ts
@@ -2,6 +2,15 @@ import { Metadata } from './types';
 
 const apiURL = 'https://contributions.guardianapis.com/epic';
 
+const reminderUrl = 'https://contribution-reminders-code.support.guardianapis.com/remind-me'; // CODE
+
+const checkForErrors = (response: any) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+};
+
 export const getBodyEnd = (meta: Metadata, url: string = apiURL): Promise<Response> => {
     const json = JSON.stringify(meta);
     return fetch(url, {
@@ -9,4 +18,39 @@ export const getBodyEnd = (meta: Metadata, url: string = apiURL): Promise<Respon
         headers: { 'Content-Type': 'application/json' },
         body: json,
     });
+};
+
+export const initSlot = (): void => {
+    const epicReminder = document.querySelector('#epicReminder');
+    if (epicReminder) {
+        const epicReminderSubmit = document.querySelector('#epicReminderSubmit');
+        if (epicReminderSubmit) {
+            epicReminderSubmit.addEventListener('click', () => {
+                const epicReminderInput = document.querySelector('#epicReminderInput');
+                if (epicReminderInput && epicReminderInput.value) {
+                    const values = JSON.stringify({
+                        email: epicReminderInput.value,
+                        reminderDate: epicReminderSubmit.getAttribute('data-reminder-date'),
+                        isPreContribution: true,
+                    });
+                    console.log('Reminder Form: ');
+                    console.log(values);
+                    fetch(reminderUrl, {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: values,
+                    })
+                        .then(checkForErrors)
+                        .then(response => response.json())
+                        .then(json => {
+                            if (json !== 'OK') {
+                                throw Error('Server error');
+                            }
+                            epicReminder.classList.add('submitted');
+                        })
+                        .catch(error => console.error('Error: ', error));
+                }
+            });
+        }
+    }
 };
