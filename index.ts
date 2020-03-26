56a153c609fda5b840f7951f4c7be5c525c075a4
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-03-26 11:38:32 +0000

    Exporting Preact

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+export { hydrate, render, createPortal } from 'preact';
 export { getBodyEnd } from './slots';
 export { getWeeklyArticleHistory, incrementWeeklyArticleCount } from './history';
 export { getViewLog, logView, getNumViewsInPreviousDays } from './contributions/viewLog';
