4e2303a467facc80700a7bcc3973d75a823bc019
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-04-14 14:40:20 +0100

    WIP component service

---
+++ components/Counter3.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { body, headline } from '@guardian/src-foundations/typography';
+import { space } from '@guardian/src-foundations';
+import { css } from 'emotion';
+
+type Props = {};
+
+const wrapperStyles = css`
+    border: 1px solid grey;
+    padding: 10px;
+    border-radius: 5px;
+`;
+
+const headingStyles = css`
+    ${headline.xsmall()}
+    margin-top: 0;
+    margin-bottom: ${space[3]}px;
+`;
+
+const bodyStyles = css`
+    ${body.medium()};
+    margin: 0 auto ${space[2]}px;
+`;
+
+export const Counter3: React.FC<Props> = ({}: Props) => {
+    const [counter, setCounter] = useState(1);
+
+    return (
+        <div className={wrapperStyles}>
+            <h1 className={headingStyles}>Hydrating Injected Components (3)</h1>
+            <p className={bodyStyles}>Counter: {counter}</p>
+            <button onClick={(): void => setCounter(counter + 1)}>Increment</button>
+        </div>
+    );
+};
---
+++ components/index.ts
@@ -0,0 +1,7 @@
+// Testing
+// export { Counter1, Counter2, Counter3 } from './components';
+import { Counter1 } from './Counter1';
+import { Counter2 } from './Counter2';
+import { Counter3 } from './Counter3';
+
+export { Counter1, Counter2, Counter3 };
