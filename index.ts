2a582316279aca537d7c97b01d75bc93eb006fb8
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-05-15 17:36:09 +0100

    Exports Preact.render

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export { getBodyEnd } from './slots';
 export { getWeeklyArticleHistory, incrementWeeklyArticleCount } from './history';
 export { getViewLog, logView, getNumViewsInPreviousDays } from './contributions/viewLog';
 export { compareVariantDecision } from './contributions/compare';
+export { render } from 'preact';
